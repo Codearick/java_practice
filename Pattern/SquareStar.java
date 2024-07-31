bbde8f103d41739abe1442167b892240bf40f120
Codearick <rohan.gautam650@gmail.com>
2024-08-01 00:29:24 +0530

    add basic pattern code.

---
+++ Pattern/SquareStar.java
@@ -0,0 +1,13 @@
+// Print 4x4 star of square shape
+
+public class SquareStar {
+    public static void main(String[] args) {
+        for(int i = 0; i<4; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.print(" * ");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
