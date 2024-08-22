62ff102a4bbbe0729044af0fe828ee42aa4cf56c
Codearick <rohan.gautam650@gmail.com>
2024-08-22 10:49:25 +0530

    add changes/update

--- Arrays/MaxSubArraySum.java
+++ Arrays/MaxSubArraySum.java
@@ -1,5 +1,5 @@
 public class MaxSubArraySum {
-    public static void maxSubArraySum(int [] nums){
+    public static void maxSubArraySum(int[] nums) {
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
 
@@ -12,7 +12,7 @@ public class MaxSubArraySum {
                     currSum += nums[k];
                 }
                 // System.out.println(currSum);
-                if(currSum > maxSum){
+                if (currSum > maxSum) {
                     maxSum = currSum;
                 }
             }
@@ -20,7 +20,7 @@ public class MaxSubArraySum {
         System.out.println("Max sum using brute force: " + maxSum);
     }
 
-    public static void maxSubArraySumUsingPrefixSum(int [] nums){
+    public static void maxSubArraySumUsingPrefixSum(int[] nums) {
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
         int prefix[] = new int[nums.length];
@@ -28,16 +28,16 @@ public class MaxSubArraySum {
         prefix[0] = nums[0];
 
         for (int i = 1; i < prefix.length; i++) {
-            prefix[i] = prefix[i-1] + nums[i];
+            prefix[i] = prefix[i - 1] + nums[i];
         }
 
         for (int i = 0; i < nums.length; i++) {
             int start = i;
             for (int j = i; j < nums.length; j++) {
                 int end = j;
-                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start -1];
-                
-                if(currSum > maxSum){
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+
+                if (currSum > maxSum) {
                     maxSum = currSum;
                 }
             }
@@ -45,27 +45,26 @@ public class MaxSubArraySum {
         System.out.println("Max sum using prefix sum: " + maxSum);
     }
 
-    public static void kadanes(int[] nums){
+    public static void kadanes(int[] nums) {
         int maxSum = Integer.MIN_VALUE;
-        int currentSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            currentSum = currentSum + nums[i];
+        int currentSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currentSum += nums[i];
 
-            if(currentSum <= 0){
+            if (currentSum <= 0) {
                 currentSum = 0;
-            }
+                }
 
             maxSum = Math.max(currentSum,maxSum);
         }
-        System.out.println("Max sum using Kandane's Algorithm : "+maxSum);
+        System.out.println("Max sum using Kandane's Algorithm : " + currentSum);
     }
 
     public static void main(String[] args) {
-        int [] numbers = {-2,-4,-6,-8,-10};
+        int[] numbers = {-1, -2};
         maxSubArraySum(numbers);
         maxSubArraySumUsingPrefixSum(numbers);
         kadanes(numbers);
 
     }
-    
 }
